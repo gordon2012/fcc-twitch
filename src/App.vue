77dfeafa427078a5a74723f83a44714394c8797c
Gordon Doskas <gordon2012@users.noreply.github.com>
2018-12-04 19:50:35 -0700

    Add styles and API calls
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,138 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <div class="container">
+      <input v-model="searchTerm" v-on:keyup.enter="search" placeholder="Search...">
+      <div class="users">
+        <user v-for="(user, i) in loadedUsers()" :key="i" :user="user"/>
+      </div>
+      <div class="responses">
+        <h3>Responses</h3>
+        <div v-for="(user, i) in users" :key="i" class="response">
+          <h4>{{ user.name }}</h4>
+          <pre><code>
+          {{ user }}
+        </code></pre>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import User from './components/User';
+
+const url = 'https://wind-bow.glitch.me/twitch-api';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    User
+  },
+  methods: {
+    loadedUsers: function() {
+      return this.users.filter(user => {
+        return !!user.user;
+      });
+    },
+    search() {
+      this.users.forEach(user => {
+        fetch(`${url}/users/${user.name}`)
+          .then(response => response.json())
+          .then(data => {
+            user.user = data;
+            this.$forceUpdate();
+
+            if (data.status !== 404) {
+              fetch(`${url}/streams/${user.name}`)
+                .then(response => response.json())
+                .then(data => {
+                  user.stream = data;
+                  this.$forceUpdate();
+                });
+            }
+          });
+      });
+    }
+  },
+  data() {
+    return {
+      searchTerm: '',
+      users: [
+        { name: 'ESL_SC2' },
+        { name: 'OgamingSC2' },
+        { name: 'cretetion' },
+        { name: 'freecodecamp' },
+        { name: 'storbeck' },
+        { name: 'habathcx' },
+        { name: 'RobotCaleb' },
+        { name: 'noobs2ninjas' },
+        { name: 'zijdijllalalal' }
+      ],
+      responses: {}
+    };
+  },
+  mounted() {
+    this.search();
+    this.$watch('user', user => {
+      console.log(user);
+    });
   }
-}
+};
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+h4 {
+  margin: 0;
+}
+pre {
+  white-space: pre-wrap;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background: #eee;
+}
+
+.container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
+.users {
+  margin-bottom: 1em;
+}
+
+.user-top {
+  border: 3px solid blue;
+  display: flex;
+}
+
+.user-top-copy {
+  padding: 0 1em;
+  flex: 1;
+  display: flex;
+  align-items: center;
+}
+
+.user-bottom {
+  border: 3px solid lime;
+  padding: 1em;
+}
+
+.responses {
+  border: 3px solid magenta;
+  padding: 1em;
+}
+
+.response {
+  border: 1px solid red;
+  padding: 1em;
+  margin-bottom: 0.5em;
 }
 </style>
---
+++ src/components/User.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="user">
+    <div class="heading">
+      <img v-if="user.user && !user.user.error" :src="user.user.logo">
+      <img v-else src="http://placeskull.com/100/100">
+      <h2 v-if="user.user.error">{{user.name}}: User Not Found</h2>
+      <h2 v-else>{{user.user.display_name}}</h2>
+    </div>
+    <div v-if="user.user && !user.user.error" class="body">
+      <div class="bio">
+        <p>{{user.user.bio}}</p>
+      </div>
+
+      <div v-if="user.stream && user.stream.stream !== null" class="stream">
+        <div>
+          <h4>Stream:</h4>
+          <p>{{user.stream.stream.channel.status}}</p>
+        </div>
+        <img :src="user.stream.stream.preview.medium">
+      </div>
+      <h4 v-else>Not Streaming</h4>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'User',
+  props: ['user']
+};
+</script>
+
+<style scoped>
+.user {
+  box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.5);
+  background: white;
+  padding: 1em;
+
+  margin-bottom: 1em;
+}
+.heading {
+  display: flex;
+  align-items: center;
+}
+.heading img {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+}
+.heading h2 {
+  padding-left: 1em;
+  flex: 1;
+}
+.heading h4 {
+  padding-right: 1em;
+}
+.stream {
+  display: flex;
+}
+.stream > div {
+  flex: 1;
+}
+</style>
